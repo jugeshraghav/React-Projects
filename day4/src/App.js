cfea3650046db0e7c161b1390bdf1a5115f4294c
Jugesh Raghav <jugesh1520raghav@gmail.com>
2021-08-25 11:49:40 +0530

    day4 project added.

--- day4/src/App.js
+++ day4/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from "./Components/Data"
 
+function Card(val,index){
+  return(
+    <>
+    <div key={index}>
+      <h1>{val.name}</h1>
+      <p>{val.location}</p>
+      <p>{val.car}</p>
+      </div>
+      </>
+  )
+ 
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>{
+   Data.map(Card)
+ }
+ </>
   );
 }
 
