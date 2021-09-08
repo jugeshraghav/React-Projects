cc67dbd99544949c7996cd31dba25a49ea019b85
Jugesh Raghav <jugesh1520raghav@gmail.com>
2021-09-08 11:27:07 +0530

    to do list added.

--- to_do/src/Components/App.js
+++ to_do/src/Components/App.js
@@ -1,39 +1,52 @@
 import React,{useState} from "react";
+import "../index.css"
 
 const App = ()=>{
     const [work, setWork]= useState("");
     const [array , setArray]=useState([]);
     const setBackWork=(event)=>{
         setWork(event.target.value);
+     
     }
     const setWorkOnclick=()=>{
-        setArray((oldItems)=>{
-         return [...oldItems,work]
-        });
+        if(work){
+            setArray((oldItems)=>{
+                return [...oldItems,work]
+               });
+        }
+        else{
+            console.log("enter a valid value");
+        }
         setWork("");
     }
-    const deleteItem = ()=>{
-console.log("deleted");
+    const deleteItem = (index)=>{
+        setArray((oldItems)=>{
+            return oldItems.filter((arrElem,id)=>{
+             return id !== index ;
+            })
+           });
     }
     return(
         <>
-        <h1>To-do List</h1>
+        <div className="container">
+        <h1>THINGS TO DO :</h1>
         <input type="text" placeholder="Enter your work"
         value={work} onChange={setBackWork}></input>
-        <button onClick={setWorkOnclick}>+</button>
-        <ol>
+        <button className="addButton" onClick={setWorkOnclick}>+</button>
+        <ul>
         {
             array.map((item,index)=>{
             return(<>
-            <div key={index} id={index}>
-            <button onClick={deleteItem}>-</button>
+            <div className="item" key={index} id={index}>
+            <button className="deleteButton" onClick={()=>{deleteItem(index)}}>-</button>
             <li>{item}</li>
             </div>
             </>
             ) 
             })
         }
-        </ol>
+        </ul>
+        </div>
         </>
     )
 }
